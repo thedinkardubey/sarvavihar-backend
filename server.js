da72fc985ad6df1fa35718abf9a205bf8f6587d8
Dinkar Dubey <138066118+thedinkardubey@users.noreply.github.com>
2025-09-12 14:47:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: 'https://sarvavihar.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
